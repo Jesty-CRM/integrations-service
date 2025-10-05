3368ebc50649b6ca16ff43e6e4e0f7aa54bb8dbd
Prashant Singh <prashantsh7014@gmail.com>
2025-10-05 12:53:34 +0530

    update cors

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(helmet());
 app.use(cors({
   origin: '*',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
